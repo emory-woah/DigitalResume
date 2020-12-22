541e9ca881d0afe0ccad742b905646c0359b55fb
oceanhymn <alex.wojtach@purchase.edu>
2020-12-22 01:12:57 -0500

    version 0.2

--- emory/main.js
+++ emory/main.js
@@ -1,5 +1,9 @@
 $(document).ready(function(){
 
+  //Imports a library to allow the sidebar to be minimalist
+  import 'simplebar';
+  import 'simplebar/dist/simplebar.css';
+
   $("a").hover(
     function(){
 
